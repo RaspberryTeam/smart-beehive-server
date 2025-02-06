6ee8ce5bd8c2c636daa79cea4e2b50031e262ed9
RaspberryTeam <tntu404.koroliuk@gmail.com>
2025-02-06 12:22:21 +0200

    chore: remove src/routers directory

--- src/routers/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-module.exports = (userController) => {
-    const router = express.Router();
-    router.post('/', (req, res) => userController.register(req, res));
-    router.post('/login', (req, res) => userController.login(req, res))
-    return router;
-};
