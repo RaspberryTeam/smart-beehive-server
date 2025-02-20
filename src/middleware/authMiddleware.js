58ac86b05f11cd6dd9ec8acc06bb37fc28b18c28
RaspberryTeam <tntu404.koroliuk@gmail.com>
2025-02-20 15:44:41 +0200

    fix: authentication middleware is fixed

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,18 +4,23 @@ module.exports = function (req, res, next) {
     
     if (req.method === "OPTIONS") {
         next()
-    }
+    };
 
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: "No auth" });
-        }
+        };
 
         const decoded = verifyToken(token);
+
+        if(!decoded) {
+            return res.status(401).json({ message: "No auth" });
+        };
+
         req.user = decoded;
         next();
     } catch (error) {
         res.status(401).json({ message: "No auth" });
-    }
+    };
 };
